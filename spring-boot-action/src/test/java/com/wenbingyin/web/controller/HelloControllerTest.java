1ed831113788e6343a3b5024406e2508ebe242cd
wenbingyin <wenbingyin@qq.com>
2020-10-22 11:01:58 +0800

    《Spring Cloud微服务实战》第2章 微服务构建：Spring Boot

---
+++ spring-boot-action/src/test/java/com/wenbingyin/web/controller/HelloControllerTest.java
@@ -0,0 +1,37 @@
+package com.wenbingyin.web.controller;
+
+import com.wenbingyin.web.MainApp;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MainApp.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("Hello World")));
+
+    }
+}
